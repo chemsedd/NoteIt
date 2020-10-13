3936fd62ece9b80c0c12fcf3d2191829e617e6bd
Chems Eddine Senoussi <chemsedd@outlook.fr>
2020-10-13 16:50:21 +0100

    💻 Created UI (mainView)

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,14 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/mainView.fxml"));
+        primaryStage.setTitle("NoteIt");
+        primaryStage.initStyle(StageStyle.DECORATED);
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
 
